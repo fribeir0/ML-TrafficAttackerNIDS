029f9d13fadd791ba95ac3794273a1228dc2d1d3
felipe <felipecarv971@gmail.com>
2026-05-01 14:06:44 -0300

    identation fix

--- c2/fake_c2.py
+++ c2/fake_c2.py
@@ -1,19 +1,25 @@
-importsocket
+import socket
 
-HOST="0.0.0.0"
-PORT=8080
+HOST = "0.0.0.0"
+PORT = 8080
 
-server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server.bind((HOST,PORT))
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
 server.listen(20)
 
 print("[C2 FAKE] Servidor iniciado na porta 8080")
 
-whileTrue:
-conn,addr=server.accept()
-data=conn.recv(4096)
+while True:
+    conn, addr = server.accept()
+    
+    try:
+        data = conn.recv(4096)
 
-ifdata:
-print(f"[C2 FAKE] Dados recebidos de{addr}:{data.decode(errors='ignore')}")
-
-conn.close()
+        if data:
+            print(f"[C2 FAKE] Dados recebidos de {addr}: {data.decode(errors='ignore')}")
+    
+    except Exception as e:
+        print(f"[ERRO] {e}")
+    
+    finally:
+        conn.close()
